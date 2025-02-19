8f20c9ffe73d43f10876601f044dea4a60516bd0
dan1kali <dl74@rice.edu>
2025-02-19 13:34:31 -0600

    added setup code for BLE instantiation and services for read and write characters

--- BluetoothUltrasoundSensor.ino/BluetoothUltrasoundSensor.ino.ino
+++ BluetoothUltrasoundSensor.ino/BluetoothUltrasoundSensor.ino.ino
@@ -1,4 +1,9 @@
 
+#include <ArduinoBLE.h>
+BLEService newService("180A"); // creating the service
+BLEByteCharacteristic readChar("2A57", BLERead);
+BLEByteCharacteristic writeChar("2A58", BLEWrite);
+
 const int trigPin = 8;
 const int echoPin = 10;
 /* const int greenLED = 2;
@@ -7,7 +12,26 @@ long duration;
 int distanceCm, distanceInch;
 
 void setup() {
-  // put your setup code here, to run once:
+
+  while(!Serial);
+  if (!BLE.begin()){
+  Serial.println("Waiting for ArduinoBLE");
+  while(1);
+  }
+
+  BLE.setDeviceName("Alice and Danika");
+  BLE.setAdvertisedService(newService);
+  newService.addCharacteristic(readChar);
+  newService.addCharacteristic(writeChar);
+  BLE.addService(newService);
+
+  readChar.writeValue(0);
+  writeChar.writeValue(0);
+
+  BLE.advertise();
+  Serial.println("Bluetooth device active");
+
+
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
